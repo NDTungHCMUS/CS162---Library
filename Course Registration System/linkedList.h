9f38d20e963661cfae89496bf1757d4da99d405f
quocphong230103 <vuongquocphongv@gmail.com>
2022-03-18 21:04:46 +0700

    fix(developing) fixing exception on functions and add new one

--- Course Registration System/linkedList.h
+++ Course Registration System/linkedList.h
@@ -2,13 +2,15 @@
 #include <iostream>
 #include <string.h>
 #include <fstream>
+#include "student.h"
+#include "login.h"
 using namespace std;
 
 template <typename T>
 struct Node
 {
 	T data;
-	Node<T>* pNext;
+	Node<T>* pNext = NULL;
 };
 
 template <typename T>
@@ -17,6 +19,34 @@ struct LinkedList {
     Node<T>* pTail = NULL;
 };
 
+// FUNCTIONS PROTOTYPE:
+template <typename T>
+void add(LinkedList<T>& l1, T item); // add to last of the list
+
+template<typename T>
+void addFront(LinkedList<T>& l1, T item); // add to front of the list
+
+template <typename T>
+int length(LinkedList<T> l1); // find the length of the list
+
+template <typename T>
+void addIndex(LinkedList<T>& l1, T item, int index); // add a node to a specific index
+
+template <typename T>
+void displayAll(LinkedList<T> l1, void(*outputItem)(T data)); // display all elements of the list
+
+template <typename T>
+void deallocateAll(LinkedList<T>& l1); // deallocate the list
+
+template <typename T>
+void removeFront(LinkedList<T>& l1); // remove first element
+
+template <typename T>
+void remove(LinkedList<T>& l1, int index); // remove specific node
+
+
+
+// FUNCTIONS DEFINITIONS:
 // add element behind the list
 template <typename T> 
 void add(LinkedList<T> &l1, T item) {
@@ -25,12 +55,12 @@ void add(LinkedList<T> &l1, T item) {
     if (l1.pHead == NULL) {
         l1.pHead = node;
         l1.pTail = l1.pHead;
-        cout << "Node added at head." << endl;
+        cout << "Element added at head." << endl;
         return;
     }
     l1.pTail->pNext = node;
     l1.pTail = l1.pTail->pNext;
-    cout << "Node added back." << endl;
+    cout << "Element added back." << endl;
 }
 
 // add element before the list
@@ -44,7 +74,7 @@ void addFront(LinkedList<T> &l1, T item) {
         cout << "First node added" << endl;
         return;
     }
-    node->next = l1.pHead;
+    node->pNext = l1.pHead;
     l1.pHead = node;
     cout << "Added at head" << endl;
 }
@@ -111,4 +141,81 @@ void deallocateAll(LinkedList<T> &l1) {
     }
 }
 
+// delete front
+template <typename T>
+void removeFront(LinkedList<T> &l1) {
+    if (l1.pHead == NULL) {
+        cout << "linked list is empty !" << endl;
+        return;
+    }
+    Node<T>* tmp = l1.pHead;
+    l1.pHead = l1.pHead->pNext;
+    delete tmp;
+    cout << "front item removed" << endl;
+}
 
+// remove a node at a specific index
+template <typename T>
+void remove(LinkedList<T> &l1, int index) {
+    if (l1.head == NULL) {
+        cout << "Empty list, nothing to remove." << endl;
+        return;
+    }
+    if (index >= length(l1) || index < 0) {
+        cout << "Invalid." << endl;
+        return;
+    }
+    if (index == 0) {
+        removeFront(l1);
+        cout << "item removed at index " << index << endl;
+        return;
+    }
+
+    int count = 0;
+    Node<T>* temp = l1.pHead;
+    while (temp != NULL) {
+        if (count == index - 1) {
+            temp->pNext = temp->pNext->pNext;
+            cout << "item removed at index " << index << endl;
+            break;
+        }
+        count++;
+        temp = temp->pNext;
+    }
+}
+
+// add a list of item
+template <typename T>
+void addList(LinkedList<T> l1, T(*inputItem)()) {
+    cout << "Choose your option: " << endl
+         << "1. Input." << endl
+         << "2. Exit." << endl;
+    char choice;
+    cin >> choice;
+    if (choice == '1') {
+        T temp = inputItem();
+        Node<T>* tmp = new Node<T>;
+        tmp->data = temp;
+        if (l1.pHead == NULL) {
+            l1.pHead = tmp;
+            l1.pTail = l1.pHead;
+      
+        }
+        else {
+            l1.pTail->pNext = tmp;
+            l1.pTail = l1.pTail->pNext;
+        }
+        cout << "Added, back to choice menu." << endl;
+        addList(l1, &inputItem);
+        return;
+    }
+    else if (choice == '2') {
+        cout << "Quit inputing" << endl;
+        return;
+    }
+    else {
+        cout << "Invalid, try another choice." << endl;
+        addList(l1, &inputItem);
+        return;
+    }
+}
--- Course Registration System/student.h
+++ Course Registration System/student.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "linkedList.h"
+using namespace std;
 
 
 struct Date {
