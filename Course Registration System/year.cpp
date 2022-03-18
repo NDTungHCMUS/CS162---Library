8c2f988b1103636df4af20eef89393424242488d
PhucHoang <phucchu390@gmail.com>
2022-03-18 15:31:05 +0700

    Function Needed

--- Course Registration System/year.cpp
+++ Course Registration System/year.cpp
@@ -1,1 +1,15 @@
 #include "year.h"
+
+void CreateYear(yearNode* &pHeadYear)
+{
+	yearNode* current = pHeadYear;
+	if (pHeadYear == nullptr) {
+		pHeadYear = new node;
+		pHeadYear->pNext = nullptr;
+		current = pHeadYear;
+	}
+	else {
+
+	}
+
+}
--- Course Registration System/year.h
+++ Course Registration System/year.h
@@ -1,12 +1,26 @@
 #pragma once
 #include <iostream>
 #include <string.h>
+#include "linkedList.h"
 using namespace std;
 
-struct year
-{
-	int yearStart;
-	int yearEnd;
-	
+struct Student {
+
 };
+
+struct Class {
+	Node<Student>* pHeadStudent;
+};
+
+struct yearNode {
+	int yearStart;
+	Node<Class>* pHeadClass = NULL;
+	yearNode* pNext = NULL;
+};
+
+void CreateYear(yearNode* &pHeadYear);
+void CreateClasses(Node<Class>* &pHeadClass);
+bool checkClass(Node<Class>* pHeadClass, string ClassName);
+void AddStudent(Node<Student>* pHeadStudent);
+
 
